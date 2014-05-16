d8fc84f796304b92b5094a92292ff80b60580c61
gwenn <gtreguier@gmail.com>
2014-05-16 20:32:16 +0200

    Add a note on current completion impl.

--- src/main/java/org/gnu/readline/CompletionCallback.java
+++ src/main/java/org/gnu/readline/CompletionCallback.java
@@ -1,14 +1,17 @@
 package org.gnu.readline;
 
 import com.sun.jna.Callback;
-import com.sun.jna.Memory;
-import com.sun.jna.Pointer;
 
 /**
  * @see AttemptedCompletionFunction#AttemptedCompletionFunction(org.gnu.readline.CompletionCallback)
  */
 public abstract class CompletionCallback implements Callback {
   public String invoke(String text, int state) {
+    // TODO Validate:
+    // https://groups.google.com/forum/#!msg/jna-users/Xy7Sm8Fb-Gc/CJ4DYo07NXEJ
+    // "Java Strings passed to C are converted to a native C string, which memory is valid for the duration of the native call only."
+    // http://cnswww.cns.cwru.edu/php/chet/readline/readline.html#SEC46
+    // "Each string the generator function returns as a match must be allocated with malloc(); Readline frees the strings when it has finished with them.
     return complete(text, state);
   }
 
